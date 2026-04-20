5c936d029fc556502a6a3f67a9f17eb68adc00a2
georgemgichuru <georgem.gichuru@gmail.com>
2026-04-20 17:36:38 +0300

    fixed the issues of deployment

--- backend/create_superuser.py
+++ backend/create_superuser.py
@@ -1,14 +1,14 @@
 import os
 import django
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 django.setup()
 
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
-username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "admin")
-email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "ggichuru")
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "georgem.gichuru@gmail.com")
 password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
 
 if not User.objects.filter(username=username).exists():
